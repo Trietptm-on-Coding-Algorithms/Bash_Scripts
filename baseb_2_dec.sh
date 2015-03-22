4ac3fe5efe0f3b4d912b524785ca09a6ac9787d0
nero-luci <cornel.punga@gmail.com>
2015-03-22 17:41:39 +0200

    [BASH] baseb_2_dec check if at least 2 arguments a given as input

--- baseb_2_dec.sh
+++ baseb_2_dec.sh
@@ -2,6 +2,12 @@
 # A function to convert a number 'number_in_base' from base 'base' to decimal
 function n2dec()
 {
+    if [ $# -lt 2 ]
+    then
+        echo "Usage: n2dec <base> <number_in_base>"
+        return
+    fi
+
     base=$1
     number_in_base=$2
     echo $((( $base#$number_in_base )))
